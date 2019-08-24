36e03ad961381664ca38267efc53dcad90e26cc7
Romain J <romain@gnous.eu>
2019-08-24 18:03:12 +0200

    delete(fitter): delete init and autoinstall files
    Desolé Mael, je sais que ces fichiers t'etaient chers mais ca aussi, ca
    va sauter

--- init.sh
+++
@@ -1,3 +0,0 @@
-#pip install -U "https://github.com/Rapptz/discord.py/archive/rewrite.zip#egg=discord.py[voice]"
-python3 -m pip install -U discord.py[voice]
-python3 -m pip install -r requirements.txt
