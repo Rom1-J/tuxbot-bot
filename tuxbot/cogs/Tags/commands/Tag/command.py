10692cdaf939ddab22910498616dd1ac09aff435
Romain J <romain.ordi@gmail.com>
2022-04-26 16:07:40 +0200

    feat(cogs:Tags>create): done tag creation

--- tuxbot/cogs/Tags/commands/Tag/command.py
+++ tuxbot/cogs/Tags/commands/Tag/command.py
@@ -13,21 +13,81 @@ from tuxbot.core.Tuxbot import Tuxbot
 from .ui.modal import TagCreationModal
 
 
-class TagCommand(commands.Cog, app_commands.Group, name="tag"):
+class TagCommand(commands.Cog, app_commands.Group, name="tag"):  # type: ignore
     """Manage tags"""
 
     def __init__(self, bot: Tuxbot):
         self.bot = bot
+
         super().__init__()
+
+    # =========================================================================
+    # =========================================================================
+
+    async def interaction_check(
+            self, interaction: discord.Interaction
+    ) -> bool:
+        """Run checks before processing command"""
+
+        if interaction.guild is None:
+            await interaction.response.send_message(
+                "This command can only be used in a guild"
+            )
+            return False
+
+        return True
+
+    # =========================================================================
+
+    async def on_error(
+            self,
+            interaction: discord.Interaction,
+            error: app_commands.AppCommandError
+    ) -> None:
+        """Whenever this command raise an error"""
+
+        if isinstance(error, app_commands.CheckFailure):
+            return
+
+        await interaction.response.send_message(
+            "Oops! Something went wrong.",
+            ephemeral=True
+        )
+
+        self.bot.logger.error(error)
 
     # =========================================================================
     # =========================================================================
 
     @app_commands.command(name="get", description="Print a tag")
     @app_commands.describe(name="Tag name")
-    async def _tag(self, interaction: discord.Interaction, name: str) -> None:
-        await interaction.response.send_message(name)
+    async def _tag_get(
+            self, interaction: discord.Interaction, name: str
+    ) -> None:
+        await interaction.response.send_message(f"Show {name}")
+
+    # =========================================================================
+
+    @app_commands.command(name="raw", description="Print a tag as aaw")
+    @app_commands.describe(name="Tag name")
+    async def _tag_raw(
+            self, interaction: discord.Interaction, name: str
+    ) -> None:
+        await interaction.response.send_message(f"Raw {name}")
+
+    # =========================================================================
 
     @app_commands.command(name="create", description="Create a tag")
-    async def _tag_create(self, interaction: discord.Interaction) -> None:
+    async def _tag_create(
+            self, interaction: discord.Interaction
+    ) -> None:
         await interaction.response.send_modal(TagCreationModal())
+
+    # =========================================================================
+
+    @app_commands.command(name="delete", description="Delete a tag")
+    @app_commands.describe(name="Tag name")
+    async def _tag_delete(
+            self, interaction: discord.Interaction, name: str
+    ) -> None:
+        await interaction.response.send_message(f"Delete {name}")
--- tuxbot/cogs/Tags/commands/Tag/ui/modal.py
+++ tuxbot/cogs/Tags/commands/Tag/ui/modal.py
@@ -3,41 +3,55 @@ Tags creation view
 """
 import discord
 
+from ..models.Tags import TagsModel
+
 
 class TagCreationModal(discord.ui.Modal):
     """
     Tag creation modal
     """
 
-    title = "Create tag"
+    title = "Create a tag"
 
     name = discord.ui.TextInput(
         label="Name",
         placeholder="Tag name here...",
+        max_length=20
     )
 
-    feedback = discord.ui.TextInput(
+    content = discord.ui.TextInput(
         label="Content",
         style=discord.TextStyle.long,
         placeholder="Tag content here...",
-        max_length=2000,
+        max_length=1900,
     )
 
-    async def on_submit(self, interaction: discord.Interaction):
+    # =========================================================================
+    # =========================================================================
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
         """Save tag on submit"""
 
-        await interaction.response.send_message(
-            f"Tag '{self.name.value}' successfully created!",
-            ephemeral=True
+        name = self.name.value.lower()
+        content = self.content.value
+
+        if await TagsModel.exists(name=name, guild_id=interaction.guild_id):
+            await interaction.response.send_message(
+                f"Tag '{name}' already exists.\n\n"
+                "Your content: (in case you did not copy it)\n"
+                f">>> {discord.utils.escape_markdown(content)}",
+                ephemeral=True
+            )
+            return
+
+        tag = await TagsModel.create(
+            guild_id=interaction.guild_id,
+            author_id=interaction.user.id,
+            name=name,
+            content=content
         )
 
-    async def on_error(
-            self, error: Exception, interaction: discord.Interaction
-    ) -> None:
-        """Normally doesn't happens ¯_(ツ)_/¯"""
         await interaction.response.send_message(
-            "Oops! Something went wrong.",
+            f"Tag '{tag.name}' successfully created!",
             ephemeral=True
         )
-
-        raise error
